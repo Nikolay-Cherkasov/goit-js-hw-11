1e2e64f239801a1680d4f1d557fe111a627e0aa6
Nikolay-Cherkasov <cherklim19@gmail.com>
2023-04-25 19:48:07 +0300

    styles

--- src/index.js
+++ src/index.js
@@ -54,18 +54,19 @@ function imageMarkup(images) {
         downloads,
       }) => {
         return `<div class="photo-card">
-<a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>  <div class="info">
+<a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>  
+<div class="info">
     <p class="info-item">
-      <b>Likes: ${likes}</b>
+      <b>Likes</br>${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views: ${views}</b>
+      <b>Views</br>${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments: ${comments}</b>
+      <b>Comments</br>${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads: ${downloads}</b>
+      <b>Downloads</br>${downloads}</b>
     </p>
   </div>
 </div>`;
